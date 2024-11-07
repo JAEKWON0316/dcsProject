405a60128989922951347315623020120c37108f
재권 <diwo02@naver.com>
2024-11-07 20:31:10 +0900

    시작

---
+++ src/main/java/com/dcs/dcs_project/DcsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dcs.dcs_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dcs/dcs_project/DcsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcs.dcs_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
