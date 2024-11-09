0ac91f06f9f5dfe974097c723ef11b189caec964
재권 <diwo02@naver.com>
2024-11-10 00:35:21 +0900

    수정

--- src/main/java/com/dcs/dcs_project/controller/BoardController.java
+++ src/main/java/com/dcs/dcs_project/controller/BoardController.java
@@ -3,20 +3,16 @@ package com.dcs.dcs_project.controller;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.dcs.dcs_project.dto.BoardDto;
 import com.dcs.dcs_project.service.BoardService;
--- src/main/java/com/dcs/dcs_project/service/BoardService.java
+++ src/main/java/com/dcs/dcs_project/service/BoardService.java
@@ -1,12 +1,10 @@
 package com.dcs.dcs_project.service;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
