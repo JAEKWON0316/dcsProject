15ebe3ad7027451969385d84e676e4b26855488e
yesun1234 <dptjs1106@naver.com>
2024-11-10 21:57:58 +0900

    -

--- dcs_react/src/components/Main.jsx
+++ dcs_react/src/components/Main.jsx
@@ -3,12 +3,27 @@ import { Link } from 'react-router-dom';
 import Img from '../images/img_sec1_1.png';
 import Img2 from '../images/icon_sec1_2.png'
 
+import Img3 from '../images/001.jpg';
+import Img4 from '../images/002.jpg';
+import Img5 from '../images/003.jpg';
+import Img6 from '../images/004.jpg';
+
 const Main = () => {
   const sectionRefs = useRef([]);
   const [isVisible, setIsVisible] = useState({});
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const slideInterval = useRef(null);
+  const slide = [
+    {'url' : Img3, 'content': '대전 평강의집에서 주최하는 ‘햇빛보기’에 참석'},
+    {'url' : Img4, 'content': '법인 이사장님의 기조연설'},
+    {'url' : Img5, 'content': '선화보틀 프로젝트 업무협약식에 참석'},
+    {'url' : Img6, 'content': '(주)마라톤의 후원금 전달식'}
+  ];
 
   useEffect(() => {
-    const currentSections = sectionRefs.current; // sectionRefs.current를 변수에 복사
+    const currentSections = sectionRefs.current; 
   
     const observers = currentSections.map((section, index) => {
       const observer = new IntersectionObserver(
@@ -28,15 +43,22 @@ const Main = () => {
       return observer;
     });
   
+    if(!isPlaying){
+      slideInterval.current = setInterval(nextSlide, 3000);
+    } else {
+      clearInterval(slideInterval.current);
+    }
+  
     return () => {
       observers.forEach((observer, index) => {
         if (currentSections[index]) {
           observer.unobserve(currentSections[index]);
         }
       });
+      clearInterval(slideInterval.current);
     };
-  }, []);
-
+  }, [isPlaying]);
+  
   const menuData = [
     { nav: '미래전략포럼', path: '/id=1-1', content: "청년을 위한 미래 전략 토론 및 발표" },
     { nav: 'AI 혁신위원회', path: '/id=1-2', content: "AI를 통한 청년 혁신과 역량 강화" },
@@ -44,6 +66,17 @@ const Main = () => {
     { nav: '글로벌네트워킹', path: '/id=1-5', content: "국제 교류를 통한 글로벌 협력" },
   ];
 
+  const handlePlayToggle = () => {
+    setIsPlaying((prev) => !prev);
+  };
+
+  const nextSlide = () => {
+    setCurrentIndex((prev) => (prev + 1) % slide.length);
+  }
+
+  const prevSlide = () => {
+    setCurrentIndex((prev) => (prev - 1 + slide.length) % slide.length);
+  }
   return (
     <div className='main'>
       <video
@@ -117,7 +150,7 @@ const Main = () => {
         className={`section visual3 motion ${isVisible[2] ? 'action' : ''}`}
         >
           <div className='noitce'>
-            <h4>Noitce</h4>
+            <h4 className='visual3m'>Noitce</h4>
             <ul className='ul'>
               <li>
                 <Link to='/'>
@@ -127,7 +160,7 @@ const Main = () => {
               </li>
               <li>
                 <Link to='/'>
-                  <em>공지</em>
+                  <em className='blue'>공모</em>
                   <strong>한국토지주택공사 광주전남지역본부 기간제근로자</strong>
                 </Link>
               </li>
@@ -151,29 +184,70 @@ const Main = () => {
               </li>
               <li>
                 <Link to='/'>
-                  <em>공지</em>
+                  <em className='blue'>공모</em>
                   <strong>한국토지주택공사 광주전남지역본부 기간제근로자</strong>
                 </Link>
               </li>
             </ul>
           </div>
-          <div className='active'>
+
+          <div className='actives'>
+            <h4 className='visual3m'>Active</h4>
             <div className='popupzone'>
-              <Link to>
-                <div className='swiper pop_slide'>
-                    
-                </div>
-              </Link>
+            {slide.map((image, index) => (
+  <div key={index}>
+    <div
+      className={`swiper pop_slide ${currentIndex === index ? 'active' : 'inactive'}`}
+      style={{
+        backgroundImage: `url(${image.url})`,  // image.url로 경로 참조
+        opacity: currentIndex === index ? 1 : 0,
+        transition: 'opacity 1s ease-in-out',
+        width: '100%',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+      }}
+    >
+    </div>
+    <div 
+      className={`activecomment ${currentIndex === index ? 'active' : 'inactive'}`}
+      style={{
+        opacity: currentIndex === index ? 1 : 0,
+        transition: 'opacity 1s ease-in-out',
+        position: 'absolute',
+        bottom : 0
+      }}
+    >
+      {image.content}
+    </div>
+  </div>
+))}
+
+
+
+              <div className='controls'>
+                <button type='button' className='arrow prev' onClick={prevSlide} tabIndex='0' aria-ralbel='Previous slide'>
+                  <span className='sr-only'>이전</span>
+                </button>
+                <button 
+                  type='button' 
+                  className={`con_btn stop ${!isPlaying ? 'active' : ''}`} 
+                  onClick={handlePlayToggle}
+                >
+                  <span className='sr-only'>정지</span>
+                </button>
+                <button 
+                  type='button' 
+                  className={`con_btn play ${isPlaying ? 'active' : ''}`} 
+                  onClick={handlePlayToggle}
+                >
+                  <span className='sr-only'>재생</span>
+                </button>
+                <button type='button' className='arrow next' onClick={nextSlide}>
+                  <span className='sr-only'>다음</span>
+                </button>
+              </div>
             </div>
-          </div>
-          <div className='give'>
-            <p>
-              작은 나눔이 
-              <br />
-              큰 힘이 됩니다.
-            </p> 
-            <Link to='/'>후원하기</Link>
-            <Link tp='/'>{/* 이미지 */}</Link>
           </div>
         </section>
 
