3f66d1ebd6c21bd55d43a3956cf6fee92159ebb4
재권 <diwo02@naver.com>
2024-11-17 21:48:54 +0900

    dd

--- dcs_react/src/components/RegionalOffices.jsx
+++ dcs_react/src/components/RegionalOffices.jsx
@@ -6,12 +6,27 @@ const RegionalOffices = () => {
     <div className='main_wrap'>
       <div className='intro'>
         <div className='visual'>
-          <strong className='title'>시/도지부</strong>
+          <strong className='title'>지부</strong>
           <span className='img'>
               <img src={Img} alt="" />
           </span>
         </div>
       </div>
+      <div>대전본부
+본부장 홍성호 
+(주)RPM 사내이사
+(사)대한청년을세계로 발기인
+(사)대한청년을세계로 부회장
+
+세종지부
+지부장 문찬우
+전 국회 선임비서관
+(사)대한청년을세계로 사외이사
+
+서울지부
+지부장 윤덕제
+JUDDANG 사장
+(사)대한청년을세계로 사외이사</div>
     </div>
   )
 }
