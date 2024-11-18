44d5e5ac651805f28fc8e3f77a1014ff8a388a8e
재권 <diwo02@naver.com>
2024-11-18 23:34:20 +0900

    gd

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    war  // WAR 플러그인 활성화
+    war
     id("org.springframework.boot") version "3.2.11"
     id("io.spring.dependency-management") version "1.1.6"
 }
@@ -30,10 +30,7 @@ dependencies {
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
-    
-    // Tomcat을 providedRuntime으로 설정하여 WAR 배포용으로 설정
     providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
-    
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("mysql:mysql-connector-java:8.0.32")
@@ -44,8 +41,7 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-// WAR 빌드를 위한 Spring Boot 설정
-bootWar {
-    mainClassName = "com.dcs.DcsProjectApplication"  // 메인 클래스 지정
+// Kotlin DSL로 'bootWar' 설정
+tasks.bootWar {
+    mainClass.set("com.dcs.DcsProjectApplication")  // 메인 클래스 지정
 }
-
