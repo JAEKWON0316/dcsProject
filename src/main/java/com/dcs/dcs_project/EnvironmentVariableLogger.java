8c75ed6e587c4b1472c1b6b34d0d3814d3fdf509
재권 <diwo02@naver.com>
2024-11-18 23:46:26 +0900

    dd

--- src/main/java/com/dcs/dcs_project/EnvironmentVariableLogger.java
+++ src/main/java/com/dcs/dcs_project/EnvironmentVariableLogger.java
@@ -7,15 +7,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class EnvironmentVariableLogger implements CommandLineRunner {
 
-    @Value("${JAWSDB_URL}")
-    private String jawsdbUrl;
+    @Value("${DATASOURCE_URL}")
+    private String datasourceUrl;
 
     @Value("${DATASOURCE_USERNAME}")
     private String datasourceUsername;
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("JAWSDB_URL: " + jawsdbUrl);
+        System.out.println("DATASOURCE_URL: " + datasourceUrl);
         System.out.println("DATASOURCE_USERNAME: " + datasourceUsername);
     }
 }
