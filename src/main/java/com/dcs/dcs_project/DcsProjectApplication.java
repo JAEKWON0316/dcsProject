df69e6ac9526ec9c24cd0525d2049c62c6e98a17
재권 <diwo02@naver.com>
2024-11-19 01:12:13 +0900

    배포

--- src/main/java/com/dcs/dcs_project/DcsProjectApplication.java
+++ src/main/java/com/dcs/dcs_project/DcsProjectApplication.java
@@ -4,24 +4,29 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
-public class DcsProjectApplication {
+public class DcsProjectApplication extends SpringBootServletInitializer {
 
-
-	 @PostConstruct
-    public void started() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-		
+    // WAR 배포를 위한 configure() 메서드 재정의
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DcsProjectApplication.class);
     }
 
-	public static void main(String[] args) {
+    // 애플리케이션이 시작될 때 실행될 메서드
+    @PostConstruct
+    public void started() {
+        // 기본 시간대를 Asia/Seoul로 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
-		
-		SpringApplication.run(DcsProjectApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        // 내장 서버에서 애플리케이션을 실행
+        SpringApplication.run(DcsProjectApplication.class, args);
+    }
 }
