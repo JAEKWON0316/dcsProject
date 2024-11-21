c06de5cea447538c73343d1ba3356a72c4c01d74
재권 <diwo02@naver.com>
2024-11-21 15:09:22 +0900

    이미지출력

--- src/main/java/com/dcs/dcs_project/controller/ImageController.java
+++ src/main/java/com/dcs/dcs_project/controller/ImageController.java
@@ -22,15 +22,10 @@ public class ImageController {
     private final ImageService iService;
 
     @GetMapping("/board/{dcsBoardId}")
-    public ResponseEntity<List<String>> getImagesByBoardId(@PathVariable Long dcsBoardId) {
-        // dcsBoardId에 해당하는 이미지 리스트 조회
-        List<ImageEntity> images = iService.getImagesByBoardId(dcsBoardId);
-
-        // 각 이미지의 URL을 변환하여 반환
-        List<String> imageUrls = images.stream()
-            .map(image -> iService.convertToPublicUrl(image.getImageUrl())) // 이미지 URL 변환
-            .collect(Collectors.toList());
-
-        return ResponseEntity.ok(imageUrls);
+    public ResponseEntity<List<ImageEntity>> getImagesByBoardId(@PathVariable Long dcsBoardId) {
+        List<ImageEntity> images = iService.getImagesByBoardId(dcsBoardId);  // dcsBoardId로 이미지 조회
+        images.forEach(image -> image.setImageUrl(iService.convertToPublicUrl(image.getImageUrl())));
+        return ResponseEntity.ok(images);
     }
+    
 }
